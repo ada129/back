270dc60d3f3eddae23b1ada4edc2291afa1e6200
ada129 <91654820+ada129@users.noreply.github.com>
2022-06-09 15:54:48 +0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const Pool=require('pg').Pool
+const pool=new Pool({
+    user:process.env.USER,
+    password:process.env.PASSWORD,
+    host:process.env.HOST,
+    port:process.env.PORTDB,
+    database:process.env.DATABASE,
+})
+module.exports=pool
